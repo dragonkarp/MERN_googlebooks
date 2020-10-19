80cb849f197b17ece9d31c2df629eb141a471b4f
Michael Li <dev.mike011235@gmail.com>
2020-10-18 22:07:29 -0400

    .

--- server.js
+++ server.js
@@ -2,15 +2,26 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+// // Serve up static assets (usually on heroku)
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
+// //The bug on deployment is here
+
+// routes.use(function(req, res) {
+//   //res.sendFile(path.join(__dirname, "../client/public/index.html"));
+//   app.use(express.static(path.join(__dirname, "../client/")));
+// });
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, "client/build")));
 }
 
 
